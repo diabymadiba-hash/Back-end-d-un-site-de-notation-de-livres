91eff1e572e8a7203868a806c2e09d033d15cd1d
Diaby Madiba <diaby.madibahotmail.fr>
2026-05-29 19:01:47 +0200

    Mise à jour

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+const userSchema = mongoose.Schema({// schéma de données pour les utilisateurs
+  email: { type: String, required: true, unique: true },// l'email doit être unique pour chaque utilisateur
+  password: { type: String, required: true }// le mot de passe est requis
 });
 
 module.exports = mongoose.model('User', userSchema);
